8341bdb52e212dc5c7355a8d19701b144c7bf270
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-16 23:36:34 +0200

    1-й таск

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const itemEl = document.querySelectorAll(".item");
+console.log("Number of categories:", itemEl.length);
+
+
+const h2El = document.querySelectorAll("h2")
+console.log("Category:", h2El[0].textContent);
+
+// const liEl = document.querySelectorAll("li");
+// console.log(liEl[1].textContent);
+
+
+const ulEl = document.querySelector("ul")
+console.log(ulEl.children);
