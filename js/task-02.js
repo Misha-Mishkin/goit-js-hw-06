c145e892ca4d24c74a25765a642e5ad438ec9436
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-19 18:32:40 +0200

    Решено второе задание

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
+const elements = ingredients.map(ingredients => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingredients;
+  
+  return liEl;
+})
+
+const ulEl = document.querySelector('#ingredients');
+ulEl.append(...elements);
+console.log(ulEl);
 
