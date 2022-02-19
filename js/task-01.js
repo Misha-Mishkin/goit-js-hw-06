5a161e370b673cac95f22d27d890e5fda4a1dac1
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-19 16:42:59 +0200

    Решено первое задание

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,8 @@
 const itemEl = document.querySelectorAll(".item");
 console.log("Number of categories:", itemEl.length);
 
-
-const h2El = document.querySelectorAll("h2")
-console.log("Category:", h2El[0].textContent);
-
-// const liEl = document.querySelectorAll("li");
-// console.log(liEl[1].textContent);
-
-
-const ulEl = document.querySelector("ul")
-console.log(ulEl.children);
+const listHeaders = document.querySelectorAll(".item h2");
+listHeaders.forEach(function (element) {
+    console.log(`Category: ${element.textContent}`);
+    console.log(`Elements: ${element.nextElementSibling.children.length}`);
+}); 
