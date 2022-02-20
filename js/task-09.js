4f348aa670adc52429abe916c72cc89d6063704a
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-20 19:19:20 +0200

    Решено девятое задание

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const refs = {
+  buttonEl: document.querySelector('.change-color'),
+  bodyEl: document.querySelector('body'),
+  spanColorEl: document.querySelector('.color'),
+}
+
+refs.buttonEl.addEventListener('click', onButtonChangeColor)
+
+function onButtonChangeColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanColorEl.textContent = refs.bodyEl.style.backgroundColor;
+}
