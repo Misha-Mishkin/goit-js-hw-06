8b56c215b40a46996b56b28071986bb100df7e30
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-20 15:08:41 +0200

    Четвертое задание решено

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,18 @@
 let counterValue = 0;
 
-const decrement = document.querySelector('decrement.dataset.action');
+const onButtonDecrClick = (event) => {
+    value.textContent = counterValue -= 1;
+}
 
-console.log(decrement);
+const onButtonIncrClick = (event) => {
+    value.textContent = counterValue += 1;
+}
+
+const refs = {
+    decrementBtn: document.querySelector('[data-action="decrement"]'),
+    incrementBtn: document.querySelector('[data-action="increment"]'),
+    value: document.querySelector('#value'),
+}
+
+refs.decrementBtn.addEventListener('click', onButtonDecrClick);
+refs.incrementBtn.addEventListener('click', onButtonIncrClick);
