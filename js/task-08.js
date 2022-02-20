181c8248be31e6d03b8c4302565f1638e8691c31
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-20 18:58:43 +0200

    Решено восьмое задание

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+    form: document.querySelector('.login-form'),
+}
 
+refs.form.addEventListener('submit', onFormSubmit)
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+
+}
