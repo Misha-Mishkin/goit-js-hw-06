6e06b5e21434444212981d77920591149d4d1e31
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-02-21 12:48:53 +0200

    Внес правки по седьмому заданию

--- js/task-07.js
+++ js/task-07.js
@@ -3,7 +3,7 @@ const refs = {
     textEl: document.querySelector('#text'),
 }
 
-refs.inputEl.addEventListener('change', onTextFontSize)
+refs.inputEl.addEventListener('input', onTextFontSize)
 
 function onTextFontSize() {
     refs.textEl.style.fontSize = `${event.currentTarget.value}px`;
